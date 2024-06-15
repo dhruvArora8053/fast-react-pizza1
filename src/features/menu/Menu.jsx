38c2f450ed4465198e31eca9fcf8994199dedb9c
Dhruv Arora <dhruvarora.code@gmail.com>
2024-06-15 18:48:36 +0530

    implemented loader

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,5 +1,28 @@
+import { useLoaderData } from "react-router-dom";
+import { getMenu } from "../../services/apiRestaurant";
+import MenuItem from "./MenuItem";
+
 function Menu() {
-  return <h1>Menu</h1>;
+  const menu = useLoaderData();
+  // react knows it coming direcly from the menu route, because we define loader there
+  // console.log(menu);
+
+  return (
+    <ul>
+      {menu.map((pizza) => (
+        <MenuItem pizza={pizza} key={pizza.id} />
+      ))}
+    </ul>
+  );
+}
+
+// Steps: for a loader
+//1. create a function where component lives for data fetching, it uses render as you fetch strategy unlike useState which fetches after render
+//2. provide it on the route
+//3. get the data into the component
+export async function loader() {
+  const menu = await getMenu();
+  return menu;
 }
 
 export default Menu;
